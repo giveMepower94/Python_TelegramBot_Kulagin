9884cec889e75c22c7292b05d67a452bcb2a7e8c
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-09-23 23:55:51 +0300

    first run the command start

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,8 @@
 import logging
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from secret import API_TOKEN
 from app.handlers.start import start
+from app.handlers.calendar_handler import handle_choice
 
 
 logging.basicConfig(
@@ -15,4 +16,9 @@ TOKEN = API_TOKEN
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_choice))
     app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
